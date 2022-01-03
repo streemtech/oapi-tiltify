30632585d92d4f3c67b80bc6699143cb00fb660c
Jeffrey Koehler <deef551@gmail.com>
2022-01-03 00:23:35 -0600

    add "json" files of documentation for formatting

--- main.go
+++ main.go
@@ -3,41 +3,90 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	kin "github.com/getkin/kin-openapi/openapi3"
 	"gopkg.in/yaml.v2"
 )
 
 var testJSON = `{
-	"id": 387,
-	"name": "DerpSquad",
-	"slug": "derpsquad",
-	"url": "https://tiltify.com/+derpsquad",
-	"bio": "",
-	"inviteOnly": true,
-	"disbanded": false,
-	"social": {
-	  "facebook": "tradechatonyoutube",
-	  "twitch": "tradechat",
-	  "twitter": "tradechat",
-	  "website": "",
-	  "youtube": "tradechat"
-	},
+	"id": 6256,
+	"username": "deef0000dragon1",
+	"slug": "deef0000dragon1",
+	"url": "https://tiltify.com/@deef0000dragon1",
 	"avatar": {
-	  "src": "https://asdf.cloudfront.net/asdf.jpg",
-	  "alt": "",
-	  "width": 200,
-	  "height": 200
-	}
-  }`
+		"src": "https://assets.tiltify.com/assets/default-avatar.png",
+		"alt": "",
+		"width": 200,
+		"height": 200
+	},
+	"email": "deef551@gmail.com",
+	"about": "streemtech test fundraiser",
+	"social": {
+		"facebook": null,
+		"twitch": "streemtech",
+		"twitter": "Streem_tech",
+		"website": "https://streem.tech",
+		"youtube": null,
+		"discord": null,
+		"mixer": null
+	},
+	"firstName": "Jeffrey",
+	"lastName": "Koehler",
+	"newsletter": null,
+	"confirmed": true
+}`
 
 //convert json data to an openapi schema.
 func main() {
 
+	jsonReg := regexp.MustCompile(`.*\.json`)
+	filepath.WalkDir("./data", func(path string, d fs.DirEntry, err error) error {
+
+		if !jsonReg.MatchString(path) {
+			return nil
+		}
+		fmt.Printf("Walking %s\n", path)
+
+		f, err := os.Open(path)
+		if err != nil {
+			fmt.Printf("ERROR OPEING FILE: %s", err.Error())
+		}
+		data, err := ioutil.ReadAll(f)
+		f.Close()
+		if err != nil {
+			fmt.Printf("ERROR READING FILE: %s", err.Error())
+		}
+		res, err := jsonExampleToYamlProperties(data)
+		if err != nil {
+			fmt.Printf("ERROR CONVERTING FILE: %s", err.Error())
+		}
+
+		f, err = os.Create(strings.ReplaceAll(path, ".json", ".yaml"))
+		if err != nil {
+			fmt.Printf("ERROR CREATING FILE: %s", err.Error())
+		}
+		_, err = f.Write(res)
+		if err != nil {
+			fmt.Printf("ERROR WRITING FILE: %s", err.Error())
+		}
+
+		return nil
+	})
+}
+
+func jsonExampleToYamlProperties(data []byte) (out []byte, err error) {
+
 	var dest map[string]any
-	err := json.Unmarshal([]byte(testJSON), &dest)
+
+	err = json.Unmarshal(data, &dest)
 	if err != nil {
-		panic(err)
+		return []byte{}, fmt.Errorf("Unable to unmarshal JSON: %w", err)
 	}
 
 	// fmt.Printf("%+v\n", dest)
@@ -47,20 +96,20 @@ func main() {
 
 	d, err := s.MarshalJSON()
 	if err != nil {
-		panic(err)
+		return []byte{}, fmt.Errorf("Unable to marshal results to JSON: %w", err)
 	}
 
 	dest = make(map[string]any)
 	err = json.Unmarshal(d, &dest)
 	if err != nil {
-		panic(err)
+		return []byte{}, fmt.Errorf("Unable to Unmarshal output json to map: %w", err)
 	}
 
 	res, err := yaml.Marshal(&dest)
 	if err != nil {
-		panic(err)
+		return []byte{}, fmt.Errorf("Unable to marshal to YAML: %w", err)
 	}
-	fmt.Printf("\n%s\n", res)
+	return res, nil
 }
 
 func mapToSchemas(m map[string]any) kin.Schemas {
